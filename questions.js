798615fefbcfab48479ad0c7f3673a4cdb37b875
David_Hamelin <hamelindavid@hotmail.fr>
2019-07-15 16:25:04 +0200

    correction

--- questions.js
+++ questions.js
@@ -14,6 +14,7 @@ var concatString = function (texte1, texte2) {
 }
 var afficherCar5 = function (texte) {
     return texte.slice(4, 5);
+    // return texte[4];
 }
 var afficher9Car = function (texte) {
     return texte.substr(0, 9);
@@ -26,21 +27,28 @@ var minusculeString = function (texte) {
 }
 var SupprEspaceString = function (texte) {
     return texte.slice(1, -1);
+    // return texte.trim();
+    // trimEnd et trimSt
 }
 var IsString = function (texte) {
-    if (typeof(texte).IsString) {
-        return true;
-    }
-    else {
-        console.log('le texte n\'est pas de type string');
-    }
+    // if (typeof texte === 'string')
+    // {
+    //     return true;
+    // }
+    // else {
+    //     console.log('le texte n\'est pas de type string');
+    // }
+    return typeof texte === 'string' ? true : false;
 }
 var AfficherExtensionString = function (texte) {
-    texte = texte.split('.').reverse();
-    return texte[0];
+    // texte = texte.split('.').reverse();
+    // return texte[0];
+    return texte.substring(texte.lastIndexOf('.')+1);
+    // .substr marche aussi mais à éviter
 }
 var NombreEspaceString = function (texte) {
-    return texte.split(' ').length -1; 
+    // on récupère le nombre d'éléments puis -1 pour avoir le nombre d'espaces
+    return texte.split(' ').length -1;
 }
 var InverseString = function (texte) {
    texte = texte.split('').reverse().join('');
@@ -65,8 +73,11 @@ var valeurAbsolueArray = function (array) {
     // }
     // return tableau;
     // SECONDE SOLUTION :
-    tableau = array.map(x => Math.abs(x));
-    return tableau;
+    // tableau = array.map(x => Math.abs(x));
+    // return tableau;
+    // ENCORE PLUS SIMPLE :
+    array = array.map(Math.abs);
+    return array;
 }
 var sufaceCercle = function (rayon) {
     var circle = Math.PI * Math.pow(rayon, 2);
@@ -79,6 +90,9 @@ var hypothenuse = function (ab, ac) {
     return Math.sqrt(bc);
 }
 var calculIMC = function (poids, taille) {
-    imc = poids / Math.pow(taille, 2);
-    return Math.round(imc *100)/100;
+    // imc = poids / Math.pow(taille, 2);
+    // // arrondie 2 chiffres après la virgule
+    // return Math.round(imc *100)/100;
+    // AUTRE SOLUTION :
+    return Number((poids / Math.pow(taille, 2)).toFixed(2));
 }
